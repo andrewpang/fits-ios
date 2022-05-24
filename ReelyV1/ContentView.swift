9e0cdf027d66e5d0165d64cccf884970d8eb745e
Andrew Pang <andrewpang94@gmail.com>
2022-05-23 17:50:42 -0700

    Initial Commit

---
+++ ReelyV1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ReelyV1
+//
+//  Created by Andrew Pang on 5/23/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
