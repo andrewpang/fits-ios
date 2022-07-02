09356196687eebbf2114ad795d55e2d6f01b3e71
Andrew Pang <andrewpang94@gmail.com>
2022-07-02 13:06:08 -0700

    update posts modles to be codable with firebase

---
+++ ReelyV1/Models/PostAuthorMap.swift
@@ -0,0 +1,14 @@
+//
+//  PostAuthorMap.swift
+//  ReelyV1
+//
+//  Created by Andrew Pang on 7/2/22.
+//
+
+import Foundation
+
+public struct PostAuthorMap: Codable, Hashable {
+    let displayName: String?
+    let profilePicImageUrl: String?
+    let userId: String?
+}
---
+++ ReelyV1/View Models/PostViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  PostViewModel.swift
+//  ReelyV1
+//
+//  Created by Andrew Pang on 7/2/22.
+//
+
+import Foundation
