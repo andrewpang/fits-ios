6d197bfb0e2f893dd01fc928e680ab0a1ec2cef3
Andrew Pang <andrewpang94@gmail.com>
2022-07-05 22:51:06 -0700

    create comments model

---
+++ ReelyV1/Models/CommentsModel.swift
@@ -0,0 +1,12 @@
+//
+//  CommentsModel.swift
+//  FIT(s)
+//
+//  Created by Andrew Pang on 7/5/22.
+//
+
+import Foundation
+
+public struct CommentsModel: Codable, Hashable {
+    var commentModels: [CommentModel]?
+}
