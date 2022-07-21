20ba12456e5d3c0cf0416ece315392c809fed00e
Andrew Pang <andrewpang94@gmail.com>
2022-07-21 15:58:26 -0700

    fetch users posts

--- ReelyV1/View Models/MyProfileViewModel.swift
+++ ReelyV1/View Models/MyProfileViewModel.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+import FirebaseCore
+import FirebaseFirestore
+import FirebaseFirestoreSwift
+import FirebaseAuth
+
+class MyProfileViewModel: ObservableObject {
+    
+    @Published var postsData = PostsModel()
+    
+    var postsListener: ListenerRegistration?
+    
+    private var db = Firestore.firestore()
+    
+    func fetchPosts() {
+        if (postsListener != nil) {
+            return
+        }
+        if let uid = Auth.auth().currentUser?.uid {
+            postsListener = db.collection("posts")
+                .whereField("author.userId", isEqualTo: uid)
+                .order(by: "createdAt", descending: true).addSnapshotListener { (querySnapshot, error) in
+                    guard let documents = querySnapshot?.documents else {
+                        print("No documents")
+                        return
+                    }
+
+                    var postList = [PostModel]()
+                    postList = documents.compactMap { querySnapshot -> PostModel? in
+                        return try? querySnapshot.data(as: PostModel.self)
+                    }
+                    DispatchQueue.main.async {
+                        self.postsData = PostsModel(postModels: postList)
+                    }
+            }
+        }
+    }
+}
