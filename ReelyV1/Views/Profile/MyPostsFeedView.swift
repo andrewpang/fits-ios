99252066c0b9ebb31b438c6efccfefcc10aeba16
Andrew Pang <andrewpang94@gmail.com>
2022-07-21 17:19:20 -0700

    own posts showing on profile

--- ReelyV1/Views/Profile/MyPostsFeedView.swift
+++ ReelyV1/Views/Profile/MyPostsFeedView.swift
@@ -6,10 +6,41 @@
 //
 
 import SwiftUI
+import WaterfallGrid
 
 struct MyPostsFeedView: View {
+    @StateObject var myProfileViewModel: MyProfileViewModel = MyProfileViewModel()
+    @State var postDetailViewModel: PostDetailViewModel = PostDetailViewModel(postModel: PostModel(author: PostAuthorMap(), imageUrl: "", title: "", body: "")) //Initial default value
+    @State var showPostDetailView = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationLink(destination: PostDetailView(postDetailViewModel: postDetailViewModel), isActive: $showPostDetailView) {
+            EmptyView()
+        }
+        .isDetailLink(false)
+        .onAppear {
+            self.myProfileViewModel.fetchPosts()
+        }
+        if let postModels = myProfileViewModel.postsData.postModels, !postModels.isEmpty {
+            WaterfallGrid(postModels) { post in
+                Button(action: {
+                    postDetailViewModel = PostDetailViewModel(postModel: post)
+                    showPostDetailView = true
+                }, label: {
+                    PostCardView(post: post)
+                })
+            }
+            .gridStyle(
+                columnsInPortrait: 2,
+                columnsInLandscape: 3,
+                spacing: 8,
+                animation: .none
+            )
+            .padding(EdgeInsets(top: 8, leading: 8, bottom: 8, trailing: 8))
+        } else {
+            Text("You don't have any posts yet :(")
+                .font(Font.custom(Constants.bodyFont, size: 16))
+        }
     }
 }
 
